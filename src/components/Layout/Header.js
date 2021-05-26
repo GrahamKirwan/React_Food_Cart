0671bd33619012c18962db323b8044a3451a1d34
graham.kirwan4@gmail.com <graham.kirwan4@gmail.com>
2021-05-25 23:12:11 -0400

    header components

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeaderCartButton from './HeaderCartButton';
+
+import mealsImage from '../../assets/meals.jpeg'
+import classes from './Header.module.css';
+
+export default function Header(props) {
+    return (
+        <React.Fragment>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt="Table of food"></img>
+            </div>
+        </React.Fragment>
+    )
+}
